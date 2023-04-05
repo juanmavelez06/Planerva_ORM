78d0e54fea19f9bb7f1f384977e2f46d6ce57143
juanmavelez06 <juanmavelez18@gmail.com>
2023-04-05 01:49:08 -0500

    Carpeta de files removida

--- node/routes/routes.js
+++ node/routes/routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.get("/", getAllBlogs);
 router.get("/:id", getBlog); //Esto pasa cuando tipiemos la ruta "ID" y me traiga un Dato en particular
-router.post("/", createBlog);
+router.post("/add/staff", createBlog);
 router.put("/:id", updateBlog);
 router.delete("/:id", deleteBlog);
 
