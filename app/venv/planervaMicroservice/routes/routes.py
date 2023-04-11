471c014002dd3d765d70982b9dc8763a96e368b6
juanmanuel26 <juanmanuelmoren54@gmail.com>
2023-04-10 23:41:56 -0500

    Se agregan nuevos cambios al microservicio: logs, validaciones y mensajes de error

--- app/venv/planervaMicroservice/routes/routes.py
+++ app/venv/planervaMicroservice/routes/routes.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, request, Flask, jsonify
 from werkzeug.utils import secure_filename
-from respones.responses import emptyFile, processError, alreadyExists, extensionNotAllowed, responseOkJson, requestsError
+from respones.responses import emptyFile, processError, alreadyExists, extensionNotAllowed, responseOkJson, requestsError, sizeError, removeFile
 import pandas as pd
 import os
 import requests
 import json
 import uuid
-
+import logging
 
 app = Flask(__name__)
 route = Blueprint('route',__name__)
 
+logs = []
+
+loggers = logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+logs.append(loggers)
+
+print(logs)
 
 #*Extensiones permitidas
 EXTENSIONESPERMITIDAS = {'xlsx'}
@@ -29,7 +36,7 @@ def upload():
     archivo = request.files['file']
     if archivo.filename == '':
         return emptyFile(404)
-
+    
     nombreArchivo = secure_filename(os.path.basename(archivo.filename))
     archivoId = str(uuid.uuid4())
     nombreArchivoID  = archivoId + '_' + nombreArchivo
@@ -37,37 +44,43 @@ def upload():
     
     extension = os.path.splitext(nombreArchivo)[1][1:].lower()
     if extension not in EXTENSIONESPERMITIDAS:
+        removeFile(PATHFILE, nombreArchivoID)
         return extensionNotAllowed(409)
     
     if os.path.exists(rutaArchivo):
+        removeFile(PATHFILE, nombreArchivoID)
         return alreadyExists(400)
     archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], rutaArchivo))
     print(rutaArchivo)
     print(archivo)
-    print(nombreArchivo)
+    logging.info(f'se subió el archivo correctamente {nombreArchivo}')
+    
     df = pd.read_excel(os.path.join(PATHFILE, nombreArchivoID))
-    print(df)
+    #print(df)
     data = df.to_dict(orient='records')
     jsonData = json.dumps(data)
-    print(data)
+   # print(data)
     try:
         if data:
             url = "http://127.0.0.1:3000/recibirDatos"
             headers = {'Content-Type': 'application/json'}
             response = requests.post(url,data=jsonData, headers=headers)
-            print(response.status_code)
-            print(response.content)
+            logging.info(response.status_code)
+            #print(response.content)
             try:
                 if response.ok:
                     return responseOkJson("Se envió el archivo correctamente", 200, nombreArchivoID)
-                else:
-                  return f"error al enviar los datos"
+                elif response.status_code == 413:
+                  removeFile(PATHFILE, nombreArchivoID)
+                  return sizeError(413)
             except FileExistsError as e:
+                removeFile(PATHFILE, nombreArchivoID)
                 print("error: no se pudo hacer la carga del archivo correctamente", e)
                 return processError(e)   
         else:
             print(data)
-            return requestsError(400) 
-    except FileExistsError as e:
-        print("error: no se pudo hacer la carga del archivo correctamente", e)
-        return processError(e)
+            removeFile(PATHFILE, nombreArchivoID)
+            return processError(400) 
+    except requests.exceptions.ConnectionError as e:
+        print("error: no se pudo conectar con el servidor exitosamente, intenta nuevamente más tarde", e)
+        return requestsError(e)
