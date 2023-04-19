20ca10fa71577a6057c15e7453e32af9d58e5079
InnitAlejandro <alejose1710@gmail.com>
2023-04-18 23:07:28 -0500

    Corrections first deploy

--- client/src/components/AppHeader/AppHeader.jsx
+++ client/src/components/AppHeader/AppHeader.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./index.css";
-import {AiOutlineUser} from "react-icons/ai"
-import {RiArrowDropDownLine} from "react-icons/ri"
+import { AiOutlineUser } from "react-icons/ai";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
-function AppHeader({sectionName}) {
+function AppHeader({ sectionName }) {
   return (
     <div className="header-ctn">
       <div className="section-title">
-        <h1>
-          <span>Plan</span>erva
-        </h1>
+        <h1>Planerva</h1>
         <h2>{sectionName}</h2>
       </div>
 
       <div className="header-user">
         <div className="ctn-user">
           <div>
-            <AiOutlineUser/>
+            <AiOutlineUser />
           </div>
         </div>
-        <RiArrowDropDownLine/>
+        <RiArrowDropDownLine />
       </div>
     </div>
   );
